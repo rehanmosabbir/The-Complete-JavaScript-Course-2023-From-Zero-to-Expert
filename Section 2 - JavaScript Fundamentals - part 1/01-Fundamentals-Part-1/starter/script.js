3a427ce9ed3244723bab6da704da1689e6029f81
rehanmosabbir <rehanmosabbir@gmail.com>
2023-05-25 12:52:52 +0600

    add solution of assignment values and variables

---
+++ Section 2 - JavaScript Fundamentals - part 1/01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,33 @@
+let JS = "amazing";
+// if (JS === "amazing") alert("JavaScript is FUN!");
+console.log(40 + 30 + 21 - 10);
+console.log("Matilda");
+console.log(54);
+
+let firstName = "Matilda";
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+// let 3times = 3;
+// let function = "function";
+let $function = "function";
+// let jonas&matilda = "JM";
+let jonas_matilda = "JM";
+const PI = 3.1416;
+
+let myFirstJob = "Programmer";
+let mySecondJob = "Teacher";
+
+let job1 = "Programmer";
+let job2 = "Teacher";
+
+console.log(myFirstJob);
+console.log(mySecondJob);
+
+///////////////////////////////
+//// Values and Variables
+
+let country = "Bangladesh";
+let continent = "Asia";
+let population = 170000000;
