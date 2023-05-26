33a0ce3d5f07beb56890eb2f9f5e3b9a2fda3afa
rehanmosabbir <rehanmosabbir@gmail.com>
2023-05-26 11:38:52 +0600

    add let const and var

--- Section 2 - JavaScript Fundamentals - part 1/01-Fundamentals-Part-1/starter/script.js
+++ Section 2 - JavaScript Fundamentals - part 1/01-Fundamentals-Part-1/starter/script.js
@@ -1,9 +1,11 @@
+/*
 let JS = "amazing";
 // if (JS === "amazing") alert("JavaScript is FUN!");
 console.log(40 + 30 + 21 - 10);
 console.log("Matilda");
 console.log(54);
 
+// Variable naming convention
 let firstName = "Matilda";
 console.log(firstName);
 console.log(firstName);
@@ -31,3 +33,46 @@ console.log(mySecondJob);
 let country = "Bangladesh";
 let continent = "Asia";
 let population = 170000000;
+
+
+// javascript data types
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+console.log(typeof javascriptIsFun);
+
+javascriptIsFun = "YES!";
+console.log(javascriptIsFun);
+console.log(typeof javascriptIsFun);
+
+console.log(typeof 23);
+console.log(typeof "Jonas");
+console.log(typeof true);
+
+let year;
+console.log(year);
+console.log(typeof year);
+let firstName = null;
+console.log(firstName);
+console.log(typeof null);
+
+year = 1989;
+console.log(year);
+console.log(typeof year);
+*/
+
+// let const and var
+
+let age = 34;
+age = 35;
+console.log(age);
+
+const birthYear = 1989;
+// birthYear = 1991;
+// const firstName;
+
+var firstName = "Mosabbir";
+firstName = "Matilda";
+console.log(firstName);
+
+lastName = "Rehan";
+console.log(lastName);
