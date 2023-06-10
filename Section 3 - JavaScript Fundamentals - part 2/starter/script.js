da8ce341f960d23a2da1e6d195c18786e5092174
Md. Mosabbir Hossain <rehanmosabbir@gmail.com>
2023-06-10 23:34:06 +0600

    complete section 5

--- Section 3 - JavaScript Fundamentals - part 2/starter/script.js
+++ Section 3 - JavaScript Fundamentals - part 2/starter/script.js
@@ -790,7 +790,7 @@ arrays. Log a string like 'Neighbour: Canada' for each country
 3. You will need a loop inside a loop for this. This is actually a bit tricky, so don't 
 worry if it's too difficult for you! But you can still try to figure this out anyway �
 */
-
+/*
 const listOfNeighbours = [
   ["Canada", "Mexico"],
   ["Spain"],
@@ -802,7 +802,7 @@ for (let i = 0; i < listOfNeighbours.length; i++) {
     console.log(`Neighbour: ${listOfNeighbours[i][j]}`);
   }
 }
-
+*/
 /*
 LECTURE: The while Loop
 1. Recreate the challenge from the lecture 'Looping Arrays, Breaking and Continuing', 
@@ -810,6 +810,7 @@ but this time using a while loop (call the array 'percentages3')
 2. Reflect on what solution you like better for this task: the for loop or the while
 loop?
 */
+/*
 const populations = [6, 1441, 170, 217];
 
 const percentages2 = [];
@@ -823,3 +824,142 @@ while (i < populations.length) {
 }
 
 console.log(percentages2);
+*/
+/*
+// PROBLEM :
+// We work for a company building a smart home thermometer. Our most recent task is this: "Given an array of temperatures of one day, calculate the temperature amplitude. Keep in mind that sometimes there might be a sensor error."
+
+const temperatures = [3, -2, -6, -1, "error", 9, 13, 17, 15, 14, 9, 5];
+// 1) Understanding the problem
+// -- What is temp amplitude? Answer : difference between highest and lowest temp
+// -- How to compute max and min temperatures?
+// -- What's a sensor error? And what to do?
+
+// 2) Breaking up into sub-problems
+// - How to ignore errors?
+// - Find max value in temp array
+// - Find min value in temp array
+// - Subtract min from max (amplitude) and return it
+
+const calcTempAmplitude = function (temps) {
+  let max = temps[0];
+  let min = temps[0];
+  for (let i = 0; i < temps.length; i++) {
+    let curTemp = temps[i];
+    if (typeof curTemp !== "number") continue;
+    if (curTemp > max) max = curTemp;
+    if (curTemp < min) min = curTemp;
+  }
+  // console.log(max, min);
+  return max - min;
+};
+
+const amplitude = calcTempAmplitude(temperatures);
+console.log(amplitude);
+
+// PROBLEM 2:
+// Function should now receive 2 arrays of temperatures
+
+// 1) Understanding the problem
+// -- with 2 arrays, should we implement the functionality twice? NO! Just merge two arrays
+
+// 2) Breaking up into sub-problems
+// How to merge 2 arrays?
+
+const calcTempAmplitudeNew = function (t1, t2) {
+  const temps = t1.concat(t2);
+  console.log(temps);
+  let max = temps[0];
+  let min = temps[0];
+  for (let i = 0; i < temps.length; i++) {
+    let curTemp = temps[i];
+    if (typeof curTemp !== "number") continue;
+    if (curTemp > max) max = curTemp;
+    if (curTemp < min) min = curTemp;
+  }
+  console.log(max, min);
+  return max - min;
+};
+
+const amplitudeNew = calcTempAmplitudeNew([5, 9, 10], [-1, 4, 7]);
+console.log(amplitudeNew);
+*/
+/*
+const measureKelvin = function () {
+  const measurement = {
+    type: "temp",
+    unit: "celsius",
+    // C) FIX
+    // value: Number(prompt("Degrees Celsius:")),
+    value: 10,
+  };
+  // console.log(measurement);
+  // B) FIND
+  console.table(measurement);
+  // console.log(measurement.value);
+  // console.warn(measurement.value);
+  // console.error(measurement.value);
+  const kelvin = measurement.value + 273;
+  return kelvin;
+};
+// A) IDENTIFY
+// console.log(measureKelvin());
+
+// Using a debugger
+const calcTempAmplitudeBug = function (t1, t2) {
+  const temps = t1.concat(t2);
+  console.log(temps);
+  let max = 0;
+  let min = 0;
+  for (let i = 0; i < temps.length; i++) {
+    let curTemp = temps[i];
+    if (typeof curTemp !== "number") continue;
+    if (curTemp > max) max = curTemp;
+    if (curTemp < min) min = curTemp;
+  }
+  console.log(max, min);
+  return max - min;
+};
+
+const amplitudeBug = calcTempAmplitudeBug([5, 9, 10], [3, 4, 7]);
+console.log(amplitudeBug);
+*/
+/*
+////////////////////////////
+///// Coding Challenge #1
+//////////////
+Given an array of forecasted maximum temperatures, the thermometer displays a 
+string with the given temperatures. Example: [17, 21, 23] will print "... 17ºC in 1 
+days ... 21ºC in 2 days ... 23ºC in 3 days ..."
+Your tasks:
+1. Create a function 'printForecast' which takes in an array 'arr' and logs a 
+string like the above to the console. Try it with both test datasets.
+2. Use the problem-solving framework: Understand the problem and break it up 
+into sub-problems!
+Test data:
+§ Data 1: [17, 21, 23]
+§ Data 2: [12, 5, -5, 0, 4]
+GOOD LUCK �
+ */
+
+// 1) Understanding the problem
+// -- Array transformed to string , seperated by ...
+// -- What is the X days? Answer : index + 1
+// 2) Breaking up into sub-problem
+// -- Transform array into string
+// -- Transform each element to string with ºC
+// -- String needs to contain day (index + 1)
+// -- Add ... between element and start and end of string
+// -- Log string to console
+/*
+const printForecast = function (arr) {
+  let newStr = ``;
+  for (let i = 0; i < arr.length; i++) {
+    newStr += ` ... ${arr[i]}ºC in ${i + 1} days`;
+  }
+  return newStr + ` ...`;
+};
+
+console.log(printForecast([17, 21, 23]));
+console.log(printForecast([12, 5, -5, 0, 4]));
+*/
